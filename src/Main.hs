d333524d65d8a2449201748ce67ecd9f069e7235
Gregory Travis <greg.m.travis@gmail.com>
2019-10-03 19:10:11 -0400

    clean

--- src/Main.hs
+++ src/Main.hs
@@ -18,37 +18,10 @@ instance Delta [a] (ListDelta a) where
   xs .+ (Insert i x) = (take i xs) ++ [x] ++ (drop i xs)
   (.-) = undefined  -- slow
 
-voo = [1, 2, 3] :: [Int]
-vep = Insert 2 4 :: ListDelta Int
-
 main = do
-  msp $ voo .+ vep
   msp $ ["a", "b", "c"] .+ Insert 2 "d"
   msp "hi"
   -- deltaDemo
   -- expDemo
   -- oldWebDemo
   -- bankProcess
-
-{-
-SO question
-
-{-# LANGUAGE MultiParamTypeClasses #-}
-module Main where
-
-class Foo a b where
-  bar :: a -> b -> a
-
-data Baz a = Baz a
-instance Foo Int (Baz a) where
-  bar i (Baz _) = i
-
-standaloneBar :: a -> b -> a
-standaloneBar x _ = x
-
-main = do
-  --putStrLn $ show $ bar 3 (Baz 4)          -- Can't unify
-  putStrLn $ show $ standaloneBar 3 (Baz 4)  -- Works fine
-  putStrLn $ show $ bar (3::Int) (Baz 4)     -- Works fine
-  putStrLn $ show $ ((bar 3 (Baz 4)) :: Int) -- Works fine
--}
