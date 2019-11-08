75bb3b9232f3a655dbf55f9b48bb28f458b27e19
Gregory Travis <greg.m.travis@gmail.com>
2019-11-08 18:59:07 -0500

    Delta typeclass

--- src/SimpleDelta.hs
+++ src/SimpleDelta.hs
@@ -1,3 +1,7 @@
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE UndecidableInstances #-}
+
 module SimpleDelta
 ( simpleDeltaDemo
 ) where
@@ -7,5 +11,35 @@ import qualified Debug.Trace as TR
 
 import Util 
 
+data Fun a b da db = Fun
+  { for :: a -> b
+  , rev :: b -> a -> a
+  , drev :: db -> a -> da }
+
+data ListIndexDelta a = LISet Int a
+
+data W = W
+  { ints :: [Int] }
+  deriving (Eq, Show)
+_ints :: [Int] -> W -> W
+_ints xs w = w { ints = xs }
+
+data WDints dints = WDints dints
+
+class Delta a da where
+  apply :: a -> da -> a
+
+instance Delta [a] (ListIndexDelta a) where
+  apply xs (LISet i x) = take i xs ++ [x] ++ drop (i+1) xs
+
+instance Delta [Int] dw => Delta W (WDints dw) where
+  apply w@(W { ints = ints }) (WDints dw) = w { ints = (apply ints dw) }
+
+world = W
+  { ints = [0, 1, 2, 3, 4] }
+
 simpleDeltaDemo = do
+  msp $ _ints [3, 4, 5] world
+  msp $ apply [4::Int, 5, 6] (LISet 1 (50::Int))
+  msp $ apply (W { ints = [4::Int, 5, 6] }) (WDints (LISet 1 (50::Int)))
   msp "shi"
