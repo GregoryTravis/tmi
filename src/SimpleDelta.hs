6050aa3c80e1f4230c3c18544d4a1b9bbf96273b
Gregory Travis <greg.m.travis@gmail.com>
2019-11-18 19:28:51 -0500

    composeFunFuns, punt Val

--- src/SimpleDelta.hs
+++ src/SimpleDelta.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE UndecidableInstances #-}
 
 module SimpleDelta
@@ -16,7 +17,37 @@ data Fun a b da db = Fun
   , rev :: b -> a -> a
   , drev :: db -> a -> da }
 
+composeFunFun :: Fun b c db dc -> Fun a b da db -> Fun a c da dc
+composeFunFun (Fun { for = forBC, rev = revBC, drev = drevBC }) (Fun { for = forAB, rev = revAB, drev = drevAB }) = Fun { for = forAC, rev = revAC, drev = drevAC }
+  where forAC = forBC . forAB
+        -- revAC :: c -> a -> a
+        revAC c a = revAB b a
+          where b = revBC c b'
+                b' = forAB a
+        -- drevAC :: dc -> a -> da
+        drevAC dc a = drevAB db a
+          where db = drevBC dc b
+                b = forAB a
+
+-- This isn't right because the delta that's written on the right (db) doesn't
+-- produce a (WDelta db) on the left, it produces some other delta in which the
+-- db is nested to arbitrary depth. This means that it should be (WDelta db')
+-- which means a db' in the def, and now I think I don't need this abbreviation
+-- since it's not as succinct as I had hoped.
+--data Val b db = Val W (Fun W b (WDelta db) db)
+--data Val b db = Val W (Fun W b (WDelta db') db)
+
+class Delta a da where
+  apply :: a -> da -> a
+
 data ListIndexDelta a = LISet Int a
+data ListConsDelta a = ListCons a | ListSnoc a
+
+instance Delta [a] (ListIndexDelta a) where
+  apply xs (LISet i x) = take i xs ++ [x] ++ drop (i+1) xs
+instance Delta [a] (ListConsDelta a) where
+  apply xs (ListCons x) = x : xs
+  apply xs (ListSnoc x) = xs ++ [x]
 
 data W = W
   { ints :: [Int] }
@@ -24,22 +55,58 @@ data W = W
 _ints :: [Int] -> W -> W
 _ints xs w = w { ints = xs }
 
-data WDints dints = WDints dints
+data WDelta da = WIntsDelta da
+_d_ints :: db -> a -> WDelta db
+_d_ints dInts _ = WIntsDelta dInts
 
-class Delta a da where
-  apply :: a -> da -> a
+funWInts :: Fun W [Int] (WDelta di) di
+funWInts = Fun { for, rev, drev }
+  where for = ints
+        rev = _ints
+        drev = _d_ints
 
-instance Delta [a] (ListIndexDelta a) where
-  apply xs (LISet i x) = take i xs ++ [x] ++ drop (i+1) xs
+-- This is just the identity dlens
+-- funW :: Fun W W (WDelta di) (WDelta di)
+funW :: Fun a a da da
+funW = Fun { for, rev, drev }
+  where for = id
+        rev = \w _ -> w
+        drev = \dw _ -> dw
 
-instance Delta [Int] dw => Delta W (WDints dw) where
-  apply w@(W { ints = ints }) (WDints dw) = w { ints = (apply ints dw) }
+-- Probably pointless
+funWInts' = composeFunFun funWInts funW
+
+instance Delta [Int] da => Delta W (WDelta da) where
+  apply w (WIntsDelta dInts) = w { ints = apply (ints w) dInts }
 
 world = W
   { ints = [0, 1, 2, 3, 4] }
 
+-- This whole section was trying to work with Val
+--applyFunVal :: Fun a b da db -> Val a da -> Val b db
+--applyFunVal fun (Val w fun') = Val w (composeFunFun fun fun')
+---- fun :: Fun a b da db
+---- fun' :: Fun W a (WDelta da) da
+---- (composeFunFun fun fun') :: Fun W b (WDelta db) db
+---- (composeFunFun fun fun') :: Fun W b (WDelta da) db  -- actual
+---- result :: Val b db
+
+---- doesn't work
+----composeFunFun :: Fun b c db dc -> Fun a b da db -> Fun a c da dc
+
+---- Inferred type seems wrong here: WDelta db is not what it would be? Oh wait, yes it is
+---- Fun a b da db -> Fun W a (WDelta da) da -> Fun W b (WDelta db) db
+
+---- works
+---- composeFunFun :: Fun a b da db -> Fun q a dq da -> Fun q b dq' db
+---- works
+----composeFunFun :: Fun b c db dc -> Fun a b da db -> Fun a c da' dc
+--composeFunFun = undefined
+
 simpleDeltaDemo = do
   msp $ _ints [3, 4, 5] world
   msp $ apply [4::Int, 5, 6] (LISet 1 (50::Int))
-  msp $ apply (W { ints = [4::Int, 5, 6] }) (WDints (LISet 1 (50::Int)))
+  msp $ apply (W { ints = [4::Int, 5, 6] }) (WIntsDelta (LISet 1 (50::Int)))
+  msp $ apply (W { ints = [4::Int, 5, 6] }) (WIntsDelta (ListCons (3::Int)))
+  msp $ apply (W { ints = [4::Int, 5, 6] }) (WIntsDelta (ListSnoc (7::Int)))
   msp "shi"
