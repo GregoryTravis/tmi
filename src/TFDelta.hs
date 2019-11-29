66d4eee765e10fb5ab5955754d8674cc44461a2e
Gregory Travis <greg.m.travis@gmail.com>
2019-11-29 14:35:44 -0500

    StringDelta

--- src/TFDelta.hs
+++ src/TFDelta.hs
@@ -19,15 +19,28 @@ instance Delta (ListIndexDelta a) where
   type V (ListIndexDelta a) = [a]
   apply xs (Insert i x) = take i xs ++ [x] ++ drop i xs
   apply xs (Update i x) = take i xs ++ [x] ++ drop (i+1) xs
+  apply xs (Delete i) = take i xs ++ drop (i+1) xs
 
 instance Delta (ListConsDelta a) where
   type V (ListConsDelta a) = [a]
   apply xs (Cons x) = x:xs
   apply xs (Snoc x) = xs ++ [x]
 
+data StringDelta = Prepend String | Append String
+
+instance Delta StringDelta where
+  type V StringDelta = String
+  apply s (Prepend s') = s' ++ s
+  apply s (Append s') = s ++ s'
+
+-- eg. value is [a], delta is Cons, and the internal delta is b
+
 typeFamilyDeltaDemo = do
   msp $ apply [1, 2, 3] (Insert 1 10)
+  msp $ apply [1, 2, 3] (Delete 1)
   msp $ apply [1, 2, 3] (Update 1 10)
   msp $ apply [1, 2, 3] (Cons 0)
   msp $ apply [1, 2, 3] (Snoc 4)
+  msp $ apply "hey" (Prepend "ss")
+  msp $ apply "hey" (Append "tt")
   msp "hoo"
