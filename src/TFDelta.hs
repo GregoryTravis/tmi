3d23463781012dcc51363e9d46b469f470f0580d
Gregory Travis <greg.m.travis@gmail.com>
2019-11-30 09:38:21 -0500

    a nested delta

--- src/TFDelta.hs
+++ src/TFDelta.hs
@@ -1,3 +1,6 @@
+{-# LANGUAGE FlexibleInstances #-}
+--{-# LANGUAGE PartialTypeSignatures #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeFamilies #-}
 module TFDelta (typeFamilyDeltaDemo) where
 
@@ -33,7 +36,44 @@ instance Delta StringDelta where
   apply s (Prepend s') = s' ++ s
   apply s (Append s') = s ++ s'
 
--- eg. value is [a], delta is Cons, and the internal delta is b
+-- Goal: modify an array of strings: change the second element, and change it by prepending to it
+-- s: string
+-- ds: StringDelta
+data Foo s ds = Update' Int ds
+
+-- hupdate :: [String] -> Foo String StringDelta -> [String]
+-- -- works
+-- hupdate ss (Update' i ds) = take i ss ++ [newS] ++ drop (i+1) ss
+--   where newS = apply (ss !! i) ds
+-- bag = hupdate ["asdf", "zxcv", "qwer"] (Update' 1 (Prepend "aa"))
+
+
+instance Delta ds => Delta (Foo s ds) where
+  type V (Foo s ds) = [V ds] -- crucial
+  apply ss (Update' i ds) = take i ss ++ [newS] ++ drop (i+1) ss
+    where newS = apply (ss !! i) ds
+
+--hag = apply ["asdf"::String, "zxcv", "qwer"] (Update' 1 (Prepend "aa"))
+
+--instance Delta (Foo String StringDelta) where
+--  type V (Foo String StringDelta) = [String]
+--  apply ss (Update' i ds) = take i ss ++ [newS] ++ drop (i+1) ss
+--    where newS = apply (ss !! i) ds
+
+--hag = apply ["asdf"::String, "zxcv", "qwer"] (Update' 1 (Prepend "aa"))
+----hag = apply (["asdf"::String, "zxcv", "qwer"] :: V (Foo _ StringDelta)) (Update' 1 (Prepend "aa"))
+
+
+--instance Delta (Foo s ds) where
+--  type V (Foo s ds) = [s]
+--  apply ss (Update' i ss') = take i ss -- ++ ([apply (ss !! i) ss'] :: _) ++ drop (i+1) ss
+--  --apply s (Update' i s') = take i s ++ (s !! i) ++ drop (i+1) s
+--    where q = 1
+--          a0 = ss' :: _
+--          a1 = ss :: _
+--          a2 = (ss !! 1) :: _
+--          a3 = ((apply :: V StringDelta -> StringDelta -> V StringDelta (ss !! 1) ss') :: _
+--          a4 = (apply :: _) "hey" (Prepend "ss")
 
 typeFamilyDeltaDemo = do
   msp $ apply [1, 2, 3] (Insert 1 10)
@@ -43,4 +83,6 @@ typeFamilyDeltaDemo = do
   msp $ apply [1, 2, 3] (Snoc 4)
   msp $ apply "hey" (Prepend "ss")
   msp $ apply "hey" (Append "tt")
+  msp $ apply ["one", "two", "three"] (Update' 1 (Prepend "sh"))
+  msp $ apply ["asdf", "zxcv", "qwer"] (Update' 1 (Append "aa"))
   msp "hoo"
