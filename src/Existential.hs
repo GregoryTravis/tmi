26a46ea5b349ba8fcfa986bbb81b0085c98513ef
Gregory Travis <greg.m.travis@gmail.com>
2020-04-10 18:42:19 -0400

    list delta and field

--- src/Existential.hs
+++ src/Existential.hs
@@ -33,27 +33,36 @@ data DDoubleAdd = DDoubleAdd Double
 instance DeltaOf Double DDoubleAdd where
   d .+ (DDoubleAdd dd) = d + dd
 
+data DList a = DListMod Int a | DListCons a
+instance DeltaOf [a] (DList a) where
+  xs .+ DListMod i x = take i xs ++ [x] ++ drop (i+1) xs
+  xs .+ DListCons x = x : xs
+
 data W = W { anInt :: Int
-           , aDouble :: Double }
+           , aDouble :: Double
+           , aList :: [Int] }
   deriving Show
 
-data DW = DAnInt (Delta Int) | DADouble (Delta Double)
+data DW = DAnInt (Delta Int) | DADouble (Delta Double) | DAList (Delta [Int])
 
 instance DeltaOf W DW where
   w@(W { anInt = i }) .+ DAnInt d = w { anInt = (i .+ d) }
   w@(W { aDouble = d }) .+ DADouble dd = w { aDouble = (d .+ dd) }
+  w@(W { aList = xs }) .+ DAList dxs = w { aList = (xs .+ dxs) }
 
 -- Can generate this for each field of W
 _dAnInt :: DeltaOf Int di => W -> di -> W
 _dAnInt w di = w .+ (DAnInt (Delta di))
 _dADouble :: DeltaOf Double dd => W -> dd -> W
 _dADouble w dd = w .+ (DADouble (Delta dd))
+_dAList w dl = w .+ (DAList (Delta dl))
 
 -- Alternately, lift (aka reverse transform) the delta separately
 -- Also generated automatically for each field in W
 toDAnInt :: Delta Int -> Delta W
 toDAnInt di = Delta (DAnInt (Delta di))
 toDADouble dd = Delta (DADouble (Delta dd))
+toDAList dl = Delta (DAList (Delta dl))
   -- let dw :: DW
   --     dw = DAnInt (Delta di)
   --  in (Delta dw)
@@ -61,11 +70,13 @@ _dAnInt' :: DeltaOf Int di => W -> di -> W
 _dAnInt' w di = w .+ (toDAnInt (Delta di))
 _dADouble' :: DeltaOf Double dd => W -> dd -> W
 _dADouble' w dd = w .+ (toDADouble (Delta dd))
+_dAList' w dd = w .+ (toDAList (Delta dd))
 
 existentialMain = do
   let w :: W
       w = W { anInt = 10
-            , aDouble = 3.3 }
+            , aDouble = 3.3
+            , aList = [1, 2, 3] }
   --msp $ 3 .+ DIntAdd 2
   --msp $ 3 .+ DIntSub 2
   --msp $ w .+ DAnInt (Delta (DIntAdd 6))
@@ -90,4 +101,6 @@ existentialMain = do
   msp $ _dAnInt' w (DIntAdd 9)
   msp $ _dAnInt' w (DIntSub 2)
   msp $ _dADouble' w (DDoubleAdd 0.1)
+  msp $ [1, 2, 3] .+ (DListMod 1 20)
+  msp $ [1, 2, 3] .+ (DListCons 7)
   msp "hihi"
