175d4bd907b397a99b21e95fdb5469d269ade925
Gregory Travis <greg.m.travis@gmail.com>
2020-04-21 08:22:37 -0400

    Num, instead

--- src/Tmi.hs
+++ src/Tmi.hs
@@ -1,4 +1,6 @@
+{-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE UndecidableInstances #-}
 
 module Tmi
 ( tmiMain ) where
@@ -9,12 +11,18 @@ class V v where
   type D v
   (.+) :: v -> D v -> v
 
-data DInt = DIntAdd Int | DIntSub Int
-instance V Int where
-  type D Int = DInt
-  x .+ (DIntAdd dx) = x + dx
-  x .+ (DIntSub dx) = x - dx
+-- data DInt = DIntAdd Int | DIntSub Int
+-- instance V Int where
+--   type D Int = DInt
+--   x .+ (DIntAdd dx) = x + dx
+--   x .+ (DIntSub dx) = x - dx
+
+data DNum n = DNumAdd n | DNumSub n
+instance Num n => V n where
+  type D n = DNum n
+  x .+ (DNumAdd dx) = x + dx
+  x .+ (DNumSub dx) = x - dx
 
 tmiMain = do
-  msp $ (3::Int) .+ (DIntAdd (12::Int))
+  msp $ 3 .+ (DNumAdd 12)
   msp "hihi"
