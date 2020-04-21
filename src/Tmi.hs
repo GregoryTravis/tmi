9859014cd9a5d65ced974bc4092902d8cf8cb777
Gregory Travis <greg.m.travis@gmail.com>
2020-04-21 08:31:26 -0400

    list delta fails because of i think overlapping instances

--- src/Tmi.hs
+++ src/Tmi.hs
@@ -11,18 +11,21 @@ class V v where
   type D v
   (.+) :: v -> D v -> v
 
--- data DInt = DIntAdd Int | DIntSub Int
--- instance V Int where
---   type D Int = DInt
---   x .+ (DIntAdd dx) = x + dx
---   x .+ (DIntSub dx) = x - dx
-
 data DNum n = DNumAdd n | DNumSub n
 instance Num n => V n where
   type D n = DNum n
   x .+ (DNumAdd dx) = x + dx
   x .+ (DNumSub dx) = x - dx
 
+-- data DList a = DListUpdate Int a | DListCons a
+-- instance V [a] where
+--   type D [a] = DList a
+--   xs .+ (DListUpdate i x) = replaceAt i x
+--     where replaceAt i x xs = take i xs ++ [x] ++ drop (i+1) xs
+--   xs .+ (DListCons x) = x : xs
+
 tmiMain = do
   msp $ 3 .+ (DNumAdd 12)
+  -- msp $ [1, 2, 3] .+ (DListUpdate 1 20)
+  -- msp $ [1, 2, 3] .+ (DListCons 10)
   msp "hihi"
