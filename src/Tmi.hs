0558d6dcaa50f2763aadbcd80aa5a09f2c32760a
Gregory Travis <greg.m.travis@gmail.com>
2020-04-21 08:19:36 -0400

    dv int, but explicit types

--- src/Tmi.hs
+++ src/Tmi.hs
@@ -1,7 +1,20 @@
+{-# LANGUAGE TypeFamilies #-}
+
 module Tmi
 ( tmiMain ) where
 
 import Util
 
+class V v where
+  type D v
+  (.+) :: v -> D v -> v
+
+data DInt = DIntAdd Int | DIntSub Int
+instance V Int where
+  type D Int = DInt
+  x .+ (DIntAdd dx) = x + dx
+  x .+ (DIntSub dx) = x - dx
+
 tmiMain = do
+  msp $ (3::Int) .+ (DIntAdd (12::Int))
   msp "hihi"
