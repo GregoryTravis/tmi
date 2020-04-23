c5d938ca376b590dcfdac13a288f895ff515a3b6
Gregory Travis <greg.m.travis@gmail.com>
2020-04-23 18:22:50 -0400

    DString, encoder

--- src/Tmi.hs
+++ src/Tmi.hs
@@ -6,6 +6,8 @@
 module Tmi
 ( tmiMain ) where
 
+import Data.Char (chr, ord)
+
 import Util
 
 class Delta d where
@@ -41,6 +43,13 @@ data W = W { ints :: [Int] }
 --           , rev = \x w -> w { ints = x }
 --           , drev = \dx w -> w { ints = (ints w) .+ dx } }
 
+data DString = Prepend String | Append String
+  deriving Show
+instance Delta DString where
+  type V DString = String
+  s .+ (Prepend s') = s' ++ s
+  s .+ (Append s') = s ++ s'
+
 data DList da = DListMod Int da | DListCons (V da)
 instance Delta da => Delta (DList da) where
   type V (DList da) = [V da]
@@ -60,8 +69,20 @@ arrIndex i = F { for, rev, drev }
 (!!-) :: F da (DList db) -> Int -> F da db
 xs !!- i = arrIndex i .* xs
 
+encoder :: Int -> F DString DString
+encoder n = F { for, rev, drev }
+  where for s = map fc s
+        rev = undefined
+        drev (Prepend prefix) _ = Prepend (map rc prefix)
+        fc c = chr (ord c + n)
+        rc c = chr (ord c - n)
+
+encoderF n x = encoder n .* x
+
 tmiMain = do
   let w = W { ints = [1, 2, 3, 4] }
   msp $ [1, 2, 3, 4] .+ DListMod 1 (Full 20)
   msp $ [[1, 2, 3, 4]] .+ DListMod 0 (DListMod 1 (Full 21))
+  msp $ "asdf" .+ Prepend "zzz"
+  --msp $ encoder 3 "asdf" .+ Prepend "zzz"
   msp "hihi"
