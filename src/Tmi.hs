c6a294b1040b8fa1157471512229daacebec1f4e
Gregory Travis <greg.m.travis@gmail.com>
2020-04-23 18:09:04 -0400

    arrIndex

--- src/Tmi.hs
+++ src/Tmi.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE UndecidableInstances #-}
 
@@ -12,7 +13,6 @@ class Delta d where
   (.+) :: V d -> d -> V d
 
 data Full a = Full a
-
 instance Delta (Full a) where
   type V (Full a) = a
   x .+ Full x' = x'
@@ -29,20 +29,24 @@ data W = W { ints :: [Int] }
 --           , rev = \x w -> w { ints = x }
 --           , drev = \dx w -> w { ints = (ints w) .+ dx } }
 
+data DList da = DListMod Int da | DListCons (V da)
 instance Delta da => Delta (DList da) where
   type V (DList da) = [V da]
   xs .+ DListMod i dx = take i xs ++ [x .+ dx] ++ drop (i+1) xs
     where x = xs !! i
   xs .+ DListCons x = x : xs
 
-data DList da = DListMod Int da | DListCons (V da)
--- instance DeltaOf [a] (DList a) where
+arrIndex :: Int -> F (DList da) da
+arrIndex i = F { for, rev, drev }
+  where for = (!!i)
+        rev x xs = xs .+ DListMod i (Full x)
+        -- Or is it this?
+        -- drev dx xs = DListMod i x'
+        --   where x' = (xs !! i) .+ dx
+        drev dx xs = DListMod i dx
 
--- arrIndex :: Int -> Fun [a] a (ListIndexDelta da) da
--- arrIndex i = Fun { for, rev, drev }
---   where for = (!!i)
---         rev x xs = take i xs ++ [x] ++ drop (i+1) xs
---         drev dx _ = Update i dx
+--(!!-) :: [V da] -> Int -> F (DLIst da) da
+--x !!- i = composeFunFun (arrIndex' i) x
 
 tmiMain = do
   let w = W { ints = [1, 2, 3, 4] }
