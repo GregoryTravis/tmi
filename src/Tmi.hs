3e25aeebba528e0417911729e130cbf41692c9f5
Gregory Travis <greg.m.travis@gmail.com>
2020-04-30 13:52:40 -0400

    clean

--- src/Tmi.hs
+++ src/Tmi.hs
@@ -11,32 +11,23 @@ import Util
 class V a where
   type D a
   type instance D a = Full a
-  --data F a :: a -> da
-  -- type F a
-  -- type instance F a = Full a
   (.+) :: a -> D a -> a
-  --(.+) = undefined
   x .+ dx = unFuller dx
-  -- _ .+ (Full x') = x'
   fuller :: a -> D a
   unFuller :: D a -> a
 
 data Full a = Full a
 
---data Full a = Full a
-
 instance V Int where
   fuller x = Full x
   unFuller (Full x) = x
 
-data Wah = Wah
+data Empty = Empty
   deriving Show
-data DWah = FDWah Wah
 
-instance V Wah where
-  type D Wah = DWah
-  fuller wah = FDWah wah
-  unFuller (FDWah wah) = wah
+instance V Empty where
+  fuller x = Full x
+  unFuller (Full x) = x
 
 -- D a == da
 data DList a da = DListMod Int da | DListCons a | Prepend [a] | Append [a] | FList [a]
@@ -58,14 +49,9 @@ instance V a => V [a] where
 (<--) :: V a => a -> a -> a
 x <-- y = x <-+ (fuller y)
 
-arf = [1::Int, 2, 3] .+ Append [4, 5]
---arf = [1::Int, 2, 3] .+ undefined
---arf = DListMod 1 20
-
 tmiMain = do
-  msp $ [Wah, Wah, Wah] .+ DListMod 1 (fuller Wah)
-  msp $ [Wah, Wah, Wah] <-+ DListMod 1 (fuller Wah)
-  msp $ [Wah, Wah, Wah] <-- [Wah, Wah]
+  msp $ [Empty, Empty, Empty] .+ DListMod 1 (fuller Empty)
+  msp $ [Empty, Empty, Empty] <-+ DListMod 1 (fuller Empty)
+  msp $ [Empty, Empty, Empty] <-- [Empty, Empty]
   msp $ [1::Int, 2, 3] .+ Append [4, 5]
---arf = [1::Int, 2, 3] .+ undefined
   msp "hihi"
