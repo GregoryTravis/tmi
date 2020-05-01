26906c4ba8f3510d2fe98521a82bd19b8eb61994
Gregory Travis <greg.m.travis@gmail.com>
2020-05-01 17:58:43 -0400

    Compound, etc

--- src/Tmi.hs
+++ src/Tmi.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE ExistentialQuantification #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE TypeApplications #-}
@@ -91,6 +92,32 @@ data DSomething a da b db = DThis da | FDThis a | DThat0 da | DThat1 (DList b db
 instance (V a, V b) => V (Something a b) where
   type D (Something a b) = DSomething a (D a) b (D b)
   This a .+ DThis da = This (a .+ da)
+  fuller = FDSomething
+  unFuller (FDSomething x) = x
+
+data EDelta a = forall da. (D a ~ da) => EDelta da | FEDelta a
+data Compound a = Compound [EDelta a] | FCompound a
+
+data ForDelta a = ForDelta a
+  deriving Show
+instance V a => V (ForDelta a) where
+  type D (ForDelta a) = EDelta a
+  ForDelta x .+ EDelta dx = ForDelta (x .+ dx)
+  fuller (ForDelta x) = FEDelta x
+  unFuller (FEDelta x) = ForDelta x
+
+data ForCompound a = ForCompound a
+  deriving Show
+unForCompound (ForCompound x) = x
+instance V a => V (ForCompound a) where
+  type D (ForCompound a) = Compound a
+  ForCompound x .+ Compound (EDelta da:das) = ForCompound (x .+ da) .+ Compound das
+  ForCompound x .+ Compound [] = ForCompound x
+  fuller (ForCompound x) = FCompound x
+  unFuller (FCompound x) = ForCompound x
+
+(.++) :: V a => a -> [EDelta a] -> a
+a .++ das = unForCompound $ ForCompound a .+ Compound das
 
 -- This is not right -- this should be (F W a) as the first arg, but it demonstrates the right issue, for now
 (<-+) :: V a => a -> D a -> a
@@ -101,31 +128,34 @@ x <-- y = x <-+ (fuller y)
 
 tmiMain = do
   let w = W { ints = [1, 2, 3, 4], strings = ["asdf", "zxcv", "qwer"] }
-  msp $ [Empty, Empty, Empty] .+ DListMod 1 (fuller Empty)
-  msp $ [Empty, Empty, Empty] <-+ DListMod 1 (fuller Empty)
-  msp $ [Empty, Empty, Empty] <-- [Empty, Empty]
-  msp $ [1::Int, 2, 3] .+ Append [4, 5]
-  msp $ (1::Int, "foo") .+ FDFst 2
-  msp $ (1::Int, "foo") .+ DSnd (DListMod 1 (fuller 'q'))
-  msp $ (1::Int, "foo") .+ DSnd (Prepend "s")
-  msp $ (1::Int, "foo") .+ FDSnd "rrr"
-  msp $ (1::Int, "foo") .+ FPair (2, "bar")
-  msp $ [([1::Int, 2, 3], ("asdf", "zxcv"))] .+ DListMod 0 (DFst (DListMod 1 (fuller (20::Int))))
-  msp $ [([1::Int, 2, 3], ("asdf", "zxcv"))] .+ DListMod 0 (DSnd (DFst (Append "g")))
-  msp $ w .+ DWInts (DListMod 2 (fuller (30::Int)))
-  msp $ w .+ DWStrings (DListMod 1 (fuller "lkjh"))
-  msp $ w .+ DWStrings (DListMod 1 (Prepend "eee"))
-  msp $ w .+ FDWInts [5, 6]
-  msp $ w .+ FDWStrings ["ttt"]
-  msp $ w .+ FDW (W { ints = [2, 3], strings = ["qwer"] })
-  let vw :: VW (Int, String) Char
-      vw = VW { as = [(1, "one")], bs = "sdfgz" }
-  msp $ vw .+ DVWAs (DListMod 0 (DFst (fuller (10::Int))))
-  msp $ vw .+ DVWBs (DListMod 0 (fuller 'S'))
-  msp $ vw .+ FDVWAs [(100, "hundred")]
-  msp $ vw .+ FDVWBs "pppp"
-  let th :: Something [Int] (Char, String)
-      th = This [1::Int, 2, 3]
-  msp $ th .+ DThis (DListMod 2 (fuller (30::Int)))
-  msp $ This @[Int] @(Char, String) [1::Int, 2, 3] .+ DThis (DListMod 2 (fuller (31::Int)))
+
+  msp $ ForCompound [1::Int, 2, 3] .+ Compound [EDelta $ DListMod 1 (fuller (10::Int)), EDelta $ Prepend [50, 60]]
+
+  -- msp $ [Empty, Empty, Empty] .+ DListMod 1 (fuller Empty)
+  -- msp $ [Empty, Empty, Empty] <-+ DListMod 1 (fuller Empty)
+  -- msp $ [Empty, Empty, Empty] <-- [Empty, Empty]
+  -- msp $ [1::Int, 2, 3] .+ Append [4, 5]
+  -- msp $ (1::Int, "foo") .+ FDFst 2
+  -- msp $ (1::Int, "foo") .+ DSnd (DListMod 1 (fuller 'q'))
+  -- msp $ (1::Int, "foo") .+ DSnd (Prepend "s")
+  -- msp $ (1::Int, "foo") .+ FDSnd "rrr"
+  -- msp $ (1::Int, "foo") .+ FPair (2, "bar")
+  -- msp $ [([1::Int, 2, 3], ("asdf", "zxcv"))] .+ DListMod 0 (DFst (DListMod 1 (fuller (20::Int))))
+  -- msp $ [([1::Int, 2, 3], ("asdf", "zxcv"))] .+ DListMod 0 (DSnd (DFst (Append "g")))
+  -- msp $ w .+ DWInts (DListMod 2 (fuller (30::Int)))
+  -- msp $ w .+ DWStrings (DListMod 1 (fuller "lkjh"))
+  -- msp $ w .+ DWStrings (DListMod 1 (Prepend "eee"))
+  -- msp $ w .+ FDWInts [5, 6]
+  -- msp $ w .+ FDWStrings ["ttt"]
+  -- msp $ w .+ FDW (W { ints = [2, 3], strings = ["qwer"] })
+  -- let vw :: VW (Int, String) Char
+  --     vw = VW { as = [(1, "one")], bs = "sdfgz" }
+  -- msp $ vw .+ DVWAs (DListMod 0 (DFst (fuller (10::Int))))
+  -- msp $ vw .+ DVWBs (DListMod 0 (fuller 'S'))
+  -- msp $ vw .+ FDVWAs [(100, "hundred")]
+  -- msp $ vw .+ FDVWBs "pppp"
+  -- let th :: Something [Int] (Char, String)
+  --     th = This [1::Int, 2, 3]
+  -- msp $ th .+ DThis (DListMod 2 (fuller (30::Int)))
+  -- msp $ This @[Int] @(Char, String) [1::Int, 2, 3] .+ DThis (DListMod 2 (fuller (31::Int)))
   msp "hihi"
