b9ddca1be80cdfc479c1c6a75e12063e1288a4da
Gregory Travis <greg.m.travis@gmail.com>
2020-09-01 16:34:22 -0400

    aString, aStringAppended

--- src/Main.hs
+++ src/Main.hs
@@ -20,9 +20,18 @@ incer = NC1 { nodeName = "adder"
             , for = (+1)
             , rev = \_ x -> x-1 }
 
-data W = W { anInt :: Int }
+appender :: NC1 String String
+appender = NC1 { nodeName = "appender"
+               , for = (++ "x")
+               , rev = rev' }
+  where rev' _ y | last y == 'x' = init y
+                 | otherwise = error "appender"
+
+data W = W { anInt :: Int
+           , aString :: String }
 w :: W
-w = W { anInt = 10 }
+w = W { anInt = 10
+      , aString = "asdf" }
 
 --_anInt :: NC1 W Int
 _anInt :: V Int
@@ -30,9 +39,17 @@ _anInt = NC1 { nodeName = "_anInt"
              , for = anInt
              , rev = \w i -> w { anInt = i } }
 
+_aString :: V String
+_aString = NC1 { nodeName = "_aString"
+               , for = aString
+               , rev = \w s -> w { aString = s } }
+
 --anIntInc :: NC1 W Int
 anIntInc :: V Int
 anIntInc = applyNC1 incer _anInt
+
+aStringAppended :: V String
+aStringAppended = applyNC1 appender _aString
 
 applyNC1 :: NC1 b c -> NC1 a b -> NC1 a c
 applyNC1 (NC1 nameBC forBC revBC) (NC1 nameAB forAB revAB) = NC1 nameAC forAC revAC
@@ -48,4 +65,5 @@ main = do
   msp $ r _anInt w
   msp $ r anIntInc w
   msp $ r (applyNC1 incer anIntInc) w
+  msp $ r aStringAppended w
   msp "hi"
