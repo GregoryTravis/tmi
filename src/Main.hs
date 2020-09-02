065c5c593c7ed02f4cf3d58ace9832df66f8d54f
Gregory Travis <greg.m.travis@gmail.com>
2020-09-02 16:17:40 -0400

    inf loop

--- src/Main.hs
+++ src/Main.hs
@@ -1,8 +1,99 @@
+--{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE ExistentialQuantification #-}
 {-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE StandaloneDeriving #-}
 
 module Main where
 
+import Data.Dynamic
+import Data.Hashable
+import qualified Data.Map.Strict as M
+import Data.Maybe (fromJust)
+import qualified Debug.Trace as TR
+--import GHC.Generics hiding (V1)
+
 import Util
+
+data Key = Key Int deriving (Eq, Ord, Show)
+
+class Hashable a => Keyable a where
+  toKey :: a -> Key
+  toKey x | TR.trace "toKey" False = undefined
+          | otherwise = (Key . hash) x
+
+instance Keyable Int
+
+data NamedFunction a b = NamedFunction String (a -> b)
+  --deriving Generic
+nfApply :: NamedFunction a b -> a -> b
+nfApply (NamedFunction _ f) = f
+nameOf :: NamedFunction a b -> String
+nameOf (NamedFunction s _) = s
+
+instance Hashable (NamedFunction a b) where
+  hash (NamedFunction s _) | TR.trace (show ("vog", s)) False = undefined
+                           | otherwise = hash s
+  hashWithSalt salt nf | TR.trace (show ("h1", nameOf nf)) False = undefined
+                       | otherwise = hash (salt, nf)
+
+data V1 b = forall a. (Hashable a, Typeable a) => V1 { for :: NamedFunction a b, arg1_0 :: V1 a }
+  --deriving Generic
+
+--deriving instance Generic (V1 a)
+
+instance Hashable a => Hashable (V1 a) where
+  hash V1 {..} | TR.trace (show ("h0", nameOf for)) False = undefined
+               | otherwise = hash (for, arg1_0)
+  hashWithSalt salt v@(V1 { for }) | TR.trace (show ("h2", nameOf for)) False = undefined
+                                   | otherwise = hash (salt, v)
+
+instance Hashable a => Keyable (V1 a)
+
+data Cache = Cache (M.Map Key Dynamic)
+
+readCache :: Typeable a => Cache -> Key -> Maybe a
+readCache (Cache m) k = case m M.!? k of Just dyn -> fromJust $ fromDynamic dyn
+                                         Nothing -> Nothing
+
+-- readCacheS :: (Show a, Typeable a) => Cache -> Key -> Maybe a
+-- readCacheS = readCache
+
+r1 :: (Hashable b, Typeable b) => Cache -> V1 b -> b
+r1 cache v =
+  case readCache cache (toKey v) of Just x -> x
+                                    Nothing -> apply1 cache v
+  -- case cache M.!? (toKey v) of Just dyn -> fromJust $ fromDynamic dyn
+  --                              Nothing -> apply1 v
+
+apply1 :: Cache -> V1 b -> b
+apply1 cache (V1 {..}) = nfApply for (r1 cache arg1_0)
+
+incer :: NamedFunction Int Int
+incer = NamedFunction "incer" (+1)
+
+anInt :: V1 Int
+anInt = V1 { for = NamedFunction "10" (\() -> 10), arg1_0 = error "10" }
+
+theCache :: Cache
+theCache = Cache $ M.fromList
+  [ (toKey anInt, toDyn (10::Int)) ]
+
+main = do
+  --msp $ r1 theCache anInt
+  --msp $ readCache theCache (toKey anInt)
+  -- let x :: Int
+  --     x = fromJust $ readCache theCache (toKey anInt)
+  let j :: Dynamic
+      j = fromJust $ case theCache of Cache m -> m M.!? (toKey anInt)
+  let x :: Int
+      x = fromJust $ fromDynamic j
+  --msp $ toKey anInt
+  --msp x
+  --msp $ toKey (3::Int)
+  msp "hi"
+
+{-
 
 data NC1 a b  = NC1 { nodeName :: String
                     , for :: a -> b
@@ -61,9 +152,10 @@ applyNC1 (NC1 nameBC forBC revBC) (NC1 nameAB forAB revAB) = NC1 nameAC forAC re
         -- a -> b -> a
         -- b -> c -> b
 
-main = do
+_main = do
   msp $ r _anInt w
   msp $ r anIntInc w
   msp $ r (applyNC1 incer anIntInc) w
   msp $ r aStringAppended w
   msp "hi"
+  -}
