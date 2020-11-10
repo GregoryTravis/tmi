f5305fa872d2e4b1123799beea617f2ed856771e
Gregory Travis <greg.m.travis@gmail.com>
2020-11-10 08:54:14 -0500

    t s n v

--- src/Internal.hs
+++ src/Internal.hs
@@ -1,6 +1,21 @@
+{-# LANGUAGE ExistentialQuantification #-}
+
 module Internal
 ( V(..)
+, S(..)
+, T(..)
+, N(..)
 ) where
 
-data V = V Int
-  deriving Show
+-- Tuple-ish type, including a 1-tuple option
+data T a = T a | T1 a
+
+data S i o = S { for :: i -> o, rev :: i -> o -> i }
+
+data N =
+  forall i o.
+  N { i :: i
+    , o :: o
+    , f :: S i o }
+
+data V a = V N Int
