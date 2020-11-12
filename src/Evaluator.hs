11b1216be34d6901a72554dc66fcb710a0b8d809
Gregory Travis <greg.m.travis@gmail.com>
2020-11-12 18:51:53 -0500

    evaluator skeleton

---
+++ src/Evaluator.hs
@@ -0,0 +1,23 @@
+module Evaluator () where
+
+-- TODO Import this -- need to modularize
+data Key
+
+-- This is to be an add-only and add-once map
+data Gen
+
+-- This should be in reverse chronological order, because the most common
+-- operation is appending and looking up the most recent one.
+-- Opaque type
+data History w = History [Gen]
+
+{-
+data Write = forall a. Write D a
+read :: Int -> Key -> a
+readHistory -> Key -> [a]
+mkHistory :: w -> [D] -> History w
+-- IO needed here?
+update :: History w -> [Write] -> IO (History w)
+-- Combine with update? So we can't forget?
+runListeners :: History w -> IO ()
+-}
