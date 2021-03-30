05bd4bb0971cc4b1ef989bdb2c2b8c10ead5d9a3
Gregory Travis <greg.m.travis@gmail.com>
2021-03-30 18:06:26 -0400

    recursively defined lens

--- src/Curry.hs
+++ src/Curry.hs
@@ -1,78 +1,54 @@
-{-# LANGUAGE ExistentialQuantification, GADTs, RecordWildCards #-}
+{-# LANGUAGE ExistentialQuantification, GADTs, RecordWildCards, TypeApplications, TypeFamilies #-}
 
 module Curry (curryMain) where
 
 import Util
 
-data W = W
-
 data Write = Write
-
-data R a = R
-
--- TODO other arities
-data F a b = F a b
-
-data V a where
-  VRoot :: V W
-  VConst :: a -> V a
-  VApp :: App a -> V a
-
-r :: V a -> a
-r VRoot = W
-r (VConst x) = x
-r (VApp app) = runAppForwards app
-
--- TODO inline this into V?
-data App a = forall b. App (V (F (b -> a) ((R b, b) -> a -> Writes))) (V b)
-
-app :: V (F (b -> a) ((R b, b) -> a -> Writes)) -> V b -> V a
-app vf vb = VApp $ App vf vb
-
-app' :: V (F (b -> c) ((R b, b) -> d)) -> V b -> V (F c d)
-app' = undefined
-
-data App'' c d = forall b. App'' (V (F (b -> c) ((R b, b) -> d))) (V b)
-
-runAppForwards :: App a -> a
-runAppForwards (App vf vx) = f x
-  where f = case r vf of (F f _) -> f
-        x = r vx
-
 type Writes = [Write]
 infix 8 <--
 (<--) :: Show a => R a -> a -> [Write]
 rx <-- x = [Write]
 
+-- e.g.
+-- for: a -> b -> c
+-- rev: a -> R a -> b -> R b -> c -> R c
+
+data R a = R (a -> Writes)
+
+-- TODO maybe this is just V
+data F a = F a (Rev a)
+
+app :: F (a -> b) -> F a -> F b
+-- app :: F (a -> b) (a -> R a -> b -> R b)
+--     -> F a (a -> R a)
+--     -> F b (b -> R b)
+app (F f_for f_rev) (F a_for a_rev) = undefined
+
+type family Rev a where
+  Rev (a -> b) = (a -> R a -> Rev b)
+  Rev a = a -> R a
+
 inc_for :: Int -> Int
 inc_for = (+1)
-inc_rev :: (R Int, Int) -> Int -> Writes
-inc_rev (rx, _) x =
+inc_rev :: Int -> R Int -> Int -> R Int
+inc_rev _x rx x = R $ \_ ->
   rx <-- x'
   where x' = x - 1
+inc :: F (Int -> Int)
+inc = F inc_for inc_rev
 
 plus_for :: Int -> Int -> Int
 plus_for = (+)
-plus_rev :: (R Int, Int) -> (R Int, Int) -> Int -> Writes
-plus_rev (rx, x) (ry, y) nZ =
+plus_rev :: Int -> R Int -> Int -> R Int -> Int -> R Int
+plus_rev x rx y ry nZ = R $ \_ ->
   rx <-- x' <>
   ry <-- y'
   where x' = nZ `div` 2
         y' = nZ - x'
 
-inc = VConst (F inc_for inc_rev)
-plus = VConst (F plus_for plus_rev)
-three = VConst 3
-threeInced = app inc three
-threeInced2x = app inc threeInced
-four = VConst (4::Int)
-seven = app' (app' plus three) four
-
---runFor :: App a
+plus :: F (Int -> Int -> Int)
+plus = F plus_for plus_rev
 
 curryMain = do
-  msp $ r three
-  msp $ r threeInced
-  msp $ r threeInced2x
-  msp $ case r seven of Curry.F x _ -> x
   msp "curry hi"
