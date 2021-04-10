10a7f4d0c4e4254798db55c6a5cd5f028e61ac98
Gregory Travis <greg.m.travis@gmail.com>
2021-04-10 11:15:52 -0400

    r

--- src/Curry.hs
+++ src/Curry.hs
@@ -3,57 +3,36 @@
 
 module Curry (curryMain) where
 
+import Control.Applicative
+
 import Util
 
-data Write
+data W = W
 
--- Going for this:
--- foo :: (a -> b -> Write) -> ((a -> b) -> Write)
--- foo :: (a -> b -> c -> Write) -> ((a -> b -> c) -> Write)
---
--- With the w
--- foo :: (w -> a -> b -> Write) -> (w -> (a -> b) -> Write)
--- foo :: (a -> b -> w -> Write) -> ((a -> b) -> w -> Write)
---
--- And that old fave
--- data V (a -> b) = V (a -> b) (a <-- b)
--- data a <-- b = Lala (a -> b -> a)
--- dat a <-- b == Lala (a -> b -> a)
--- a <-- (b <-- c) == Lala (a -> (b -> c -> b) -> a)
--- (a <-- b) <-- c == Lala (a -> b -> a) -> c -> (a -> b -> a)
---
--- a -> (b, b -> a)
--- a -> (b -> (c, c -> b), (b -> (c, c -> b)) -> a)
--- a -> (b -> (c, c -> (a, b)))
+data F a = F (W -> a)
+data R a = R (W -> a -> W)
 
--- TODO: no, a V can't just have an a, should at least be w -> a
-data V a = V a (a -> Write)
+data V a = V (F a) (R a)
 
--- instance Applicative V where
--- instance Functor VV where
---   fmap f (VV wa) = VV f'
---     where f' w = f (wa w)
--- 
--- instance Applicative VV where
---   pure x = VV (\_ -> x)
---   (VV wf) <*> (VV wx) = VV (\w -> (wf w) (wx w))
+instance Functor V where
+  fmap = undefined
 
-data F1 a b = F1 (a -> b) (a -> b -> Write)
+instance Applicative V where
+  pure = undefined
+  (<*>) = undefined
+  --liftA2 :: (a -> b -> c) -> f a -> f b -> f c
 
-luft1 :: F1 a b -> V (a -> b)
---    :: F1 (a -> b) (a -> b -> Write) -> V (a -> b) ((a -> b) -> Write)
-luft1 (F1 f r) = V f (luftRev1 r)
+inc :: V (Int -> Int)
+inc = undefined
 
-luftRev1 :: (a -> b -> Write) -> ((a -> b) -> Write)
--- also  :: (a -> b -> Write) ->  (a -> b) -> Write 
-luftRev1 rev huh = undefined
+lInc :: V Int -> V Int
+lInc = (inc <*>)
 
--- applicative-ish
-appy :: V (a -> b) -> V a -> V b
---   :: V (a -> b) ((a -> b) -> Write) ->
---      V a (a -> Write) ->
---      V b (b -> Write)
-appy = undefined
+plus :: V (Int -> Int -> Int)
+plus = undefined
+
+plusV :: V Int -> V Int -> V Int
+plusV = (<*>) . (plus <*>)
 
 curryMain = do
   msp "curry hi"
