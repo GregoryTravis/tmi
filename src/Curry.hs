339c97e2a61c97f7753039045c26adf0c4c8142b
Gregory Travis <greg.m.travis@gmail.com>
2021-04-10 10:51:47 -0400

    unimplementable

--- src/Curry.hs
+++ src/Curry.hs
@@ -5,203 +5,55 @@ module Curry (curryMain) where
 
 import Util
 
-data Write = forall a. Show a => Write (R a) a
---data Write = Write
-type Writes = [Write]
-infix 8 <--
-(<--) :: Show a => R a -> a -> [Write]
-rx <-- x = [Write rx x]
+data Write
 
-instance Show Write where
-  show (Write r a) = "(" ++ (show r) ++ " <-- " ++ (show a) ++ ")"
+-- Going for this:
+-- foo :: (a -> b -> Write) -> ((a -> b) -> Write)
+-- foo :: (a -> b -> c -> Write) -> ((a -> b -> c) -> Write)
+--
+-- With the w
+-- foo :: (w -> a -> b -> Write) -> (w -> (a -> b) -> Write)
+-- foo :: (a -> b -> w -> Write) -> ((a -> b) -> w -> Write)
+--
+-- And that old fave
+-- data V (a -> b) = V (a -> b) (a <-- b)
+-- data a <-- b = Lala (a -> b -> a)
+-- dat a <-- b == Lala (a -> b -> a)
+-- a <-- (b <-- c) == Lala (a -> (b -> c -> b) -> a)
+-- (a <-- b) <-- c == Lala (a -> b -> a) -> c -> (a -> b -> a)
+--
+-- a -> (b, b -> a)
+-- a -> (b -> (c, c -> b), (b -> (c, c -> b)) -> a)
+-- a -> (b -> (c, c -> (a, b)))
 
--- e.g.
--- for: a -> b -> c
--- rev: a -> R a -> b -> R b -> c -> R c
+-- TODO: no, a V can't just have an a, should at least be w -> a
+data V a = V a (a -> Write)
 
-data R a = R (V (B a))
-  deriving Show
+-- instance Applicative V where
+-- instance Functor VV where
+--   fmap f (VV wa) = VV f'
+--     where f' w = f (wa w)
+-- 
+-- instance Applicative VV where
+--   pure x = VV (\_ -> x)
+--   (VV wf) <*> (VV wx) = VV (\w -> (wf w) (wx w))
 
--- TODO maybe this is just V -- nope, where would the a come from!
-data F f r = F String f r
+data F1 a b = F1 (a -> b) (a -> b -> Write)
 
-type L f = F f (Rev f)
+luft1 :: F1 a b -> V (a -> b)
+--    :: F1 (a -> b) (a -> b -> Write) -> V (a -> b) ((a -> b) -> Write)
+luft1 (F1 f r) = V f (luftRev1 r)
 
--- TODO does this work?
--- type B a = F a (Rev a)
-type B a = F a (a -> Writes)
+luftRev1 :: (a -> b -> Write) -> ((a -> b) -> Write)
+-- also  :: (a -> b -> Write) ->  (a -> b) -> Write 
+luftRev1 rev huh = undefined
 
-type family Rev a where
-  Rev (a -> b) = (a -> R a -> Rev b)
-  Rev a = a -> Writes
-
-instance Show (F f r) where
-  show (F s f _) = show s
-
-inc_for :: Int -> Int
-inc_for = (+1)
-inc_rev :: Rev (Int -> Int)
-inc_rev _x rx x =
-  rx <-- x'
-  where x' = x - 1
--- TODO maybe put Rev back in just for declaring these, maybe a type alias too
-inc :: L (Int -> Int)
-inc = F "inc" inc_for inc_rev
-
-plus_for :: Int -> Int -> Int
-plus_for = (+)
-plus_rev :: Rev (Int -> Int -> Int)
-plus_rev x rx y ry nZ =
-  rx <-- x' <>
-  ry <-- y'
-  where x' = nZ `div` 2
-        y' = nZ - x'
-
-plus :: L (Int -> Int -> Int)
-plus = F "plus" plus_for plus_rev
-plusV = VConst plus
-
--- plus3 :: Integer -> Float -> Double -> Double
--- plus3 xi yf zd = fromInteger xi + realToFrac yf + zd
-plus3_for :: Int -> Int -> Int -> Int
-plus3_for x y z = x + y + z
-plus3_rev :: Rev (Int -> Int -> Int -> Int)
-plus3_rev x rx y ry z rz nW =
-  rx <-- x' <>
-  ry <-- y' <>
-  rz <-- z'
-  where x' = nW `div` 3
-        y' = (nW - x') `div` 2
-        z' = nW - x' - y'
-plus3V = VConst (F "plus3" plus3_for plus3_rev)
-
-data W = W { anInt :: Int, anotherInt :: Int, yetAnotherInt :: Int }
-  deriving Show
-world :: W
-world = W { anInt = 1, anotherInt = 50, yetAnotherInt = 2000 }
-
-anInt_for :: W -> Int
-anInt_for = anInt
-anInt_rev :: Rev (W -> Int)
-anInt_rev w rw nAnInt =
-  rw <-- w { anInt = nAnInt }
-anIntV :: V (L (W -> Int))
-anIntV = VConst (F "anInt" anInt_for anInt_rev)
-anotherInt_for :: W -> Int
-anotherInt_for = anotherInt
-anotherInt_rev :: Rev (W -> Int)
-anotherInt_rev w rw nanotherInt =
-  rw <-- w { anotherInt = nanotherInt }
-anotherIntV :: V (L (W -> Int))
-anotherIntV = VConst (F "anotherInt" anotherInt_for anotherInt_rev)
-yetAnotherInt_for :: W -> Int
-yetAnotherInt_for = yetAnotherInt
-yetAnotherInt_rev :: (Rev (W -> Int))
-yetAnotherInt_rev w rw nyetAnotherInt =
-  rw <-- w { yetAnotherInt = nyetAnotherInt }
-yetAnotherIntV = VConst (F "yetAnotherIntV" yetAnotherInt_for yetAnotherInt_rev)
--- TODO bad name
-anIntVV = VApp anIntV VRoot
-
--- data V a = VRoot | VConst a | forall b. VApp (V (F (b -> a))) (V (F a))
-data V a where
-  VRoot :: V (B W)
-  VConst :: F f r -> V (F f r)
-  VApp :: V (F (b -> a) (b -> R b -> c)) -> V (B b) -> V (F a c)
-
-instance Show a => Show (V a) where
-  show VRoot = "{root}"
-  show (VConst a) = show a
-  show (VApp vf vb) = "(VApp " ++ (show vf) ++ " " ++ (show vb) ++ ")"
-
-incV :: V (L (Int -> Int))
-incV = VConst inc
-threeF :: B Int
-threeF = F "three" 3 undefined
-threeV :: V (B Int)
-threeV = VConst threeF
--- TODO this should be (V (F Int)), try to do that with ~?
-four :: V (B Int)
-four = VApp incV threeV
-five = VApp incV four
-
-seven :: V (B Int)
-seven = VApp (VApp plusV threeV) four
-
-fiftyOne = VApp (VApp plusV (VApp anotherIntV VRoot)) (VApp anIntV VRoot)
-
-r :: V a -> a
-r VRoot = (F "world" world undefined)
-r (VConst x) = x
-r (VApp vf vb) =
-  let (F sF forF revF) = r vf
-      (F sx forB revB) = r vb
-      -- forF :: Int -> Int
-      -- revF :: Int -> R Int -> Int -> Writes
-      -- forB :: Int
-      -- revB :: Int -> Writes
-      --   vb :: (V (F Int (Int -> Writes)))
-      -- R vb :: R Int ~ R (V (F Int (Int -> Writes)))
-      -- rev' :: Int -> Writes
-      rev' = revF forB (R vb)
-   in F (show (sF, sx)) (forF forB) rev'
-
--- The rev of (VApp plusV threeV)
--- arg is (VApp (VApp "plus" "three") (VApp "inc" "three"))
--- F (Int -> Int -> Int) (Int -> R Int -> Int -> R Int -> Int -> Writes)
--- VApp :: V (F (b -> a) (b -> R b -> c)) -> V (F b (b -> Writes)) -> V (F a c)
-
-for :: F a b -> a
-for (F _ f _) = f
-
--- TOOD we want to write to anIntVV
-w :: V (B a) -> a -> Writes
-w VRoot _ = error "Can't write to VRoot"
-w (VConst _) _ = error "Can't write to VConst"
-w (VApp vf vb) nA =
-  -- revF :: Int -> R Int -> Int -> Writes
-  -- revB :: Int -> Writes
-  let (F _ forF revF) = r vf
-      (F _ forB revB) = r vb
-   in revF forB (R vb) nA
-
--- So far, behaves like w
-w' :: V (B a) -> a -> Writes
-w' VRoot _ = error "Can't write to VRoot"
-w' (VConst _) _ = error "Can't write to VConst"
-w' va nA =
-  let (F _ for rev) = r va
-   in rev nA
+-- applicative-ish
+appy :: V (a -> b) -> V a -> V b
+--   :: V (a -> b) ((a -> b) -> Write) ->
+--      V a (a -> Write) ->
+--      V b (b -> Write)
+appy = undefined
 
 curryMain = do
-  msp $ for $ r threeV
-  msp $ for $ r four
-  msp $ for $ r five
-  msp $ for $ r anIntVV
-  msp $ for $ r (VApp anotherIntV VRoot)
-  msp $ for $ r (VApp yetAnotherIntV VRoot)
-  msp $ for $ r seven
-  msp $ for $ r fiftyOne
-  msp $ for $ r (VApp incV fiftyOne)
-  msp "===="
-  msp $ w four 40
-  msp $ w Curry.anIntVV 99
-  msp $ w (VApp Curry.anotherIntV VRoot) 700
-  msp $ w seven 7001
-  msp $ w (VApp (VApp plusV anIntVV) (VApp anotherIntV VRoot)) 101
-  msp $ w (VApp anIntV VRoot) 50
-  msp $ w (VApp anotherIntV VRoot) 51
-  msp "===="
-  msp $ w' four 40
-  msp $ w' Curry.anIntVV 99
-  msp $ w' (VApp Curry.anotherIntV VRoot) 700
-  msp $ w' seven 7001
-  msp $ w' (VApp (VApp plusV anIntVV) (VApp anotherIntV VRoot)) 101
-  msp $ w' (VApp anIntV VRoot) 50
-  msp $ w' (VApp anotherIntV VRoot) 51
-  msp "===="
-  msp $ for $ r (VApp (VApp (VApp plus3V anIntVV) (VApp anotherIntV VRoot))
-                            (VApp yetAnotherIntV VRoot))
-  msp $ w (VApp (VApp (VApp plus3V anIntVV) (VApp anotherIntV VRoot))
-                      (VApp yetAnotherIntV VRoot)) 25001
   msp "curry hi"
