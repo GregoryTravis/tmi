c44eb69eb1b77a029a4316684c61ac2f03ed0730
Gregory Travis <greg.m.travis@gmail.com>
2022-01-29 16:58:20 -0500

    old invitation schema and framework

--- src/Main.hs
+++ src/Main.hs
@@ -4,10 +4,12 @@ import System.IO
 
 import Log
 import Meta
+import Old
 import Util
 
 main = do
   hSetBuffering stdout NoBuffering
   -- metaMain logApp
-  logMain
+  -- logMain
+  oldMain
   msp "hi"
---
+++ src/Old.hs
@@ -0,0 +1,58 @@
+{-# Language NamedFieldPuns #-}
+
+module Old (oldMain) where
+
+import Core
+import Lift
+import MainLoop
+import Monad
+import Propagate
+import Ty hiding (V, Bi, R)
+import qualified Ty as Ty
+import Util
+import V
+import Veq
+
+type V = Ty.V W
+type Bi = Ty.Bi W
+type R = Ty.R W
+root :: V W
+root = VRoot
+
+data W = W
+  { invitees :: [String]
+  , invited :: [String]
+  }
+  deriving (Eq, Read, Show)
+
+theWorld :: W
+theWorld = W
+  { invitees = []
+  , invited = []
+  }
+
+binvitees :: V [String]
+binvitees = VBiSeal (BiApp (bi (VNamed "invitees" invitees) (VNamed "invitees_" invitees_)) root)
+invitees_ :: W -> R W -> R [String]
+invitees_ w wr = mkR ir
+  where ir invitees = write wr $ w { invitees }
+
+lookupCommand :: AppEnv W
+lookupCommand ["old"] = old
+
+old :: Program W
+old = toProg sdone old'
+
+old' :: Blef w ()
+old' = do
+  io $ msp "hi old'"
+  return ()
+
+oldApp = App { initialW = theWorld, appEnv = lookupCommand }
+
+oldMain = do
+  let dbdir = "old"
+  reset dbdir
+  ensureDbDir dbdir (initialW oldApp)
+  injectEvent dbdir oldApp (Command ["old"])
+  run oldApp dbdir
