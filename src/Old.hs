6f819a3dd19d406f9010e85130598bf20eabd20c
Gregory Travis <greg.m.travis@gmail.com>
2022-02-11 14:51:50 -0500

    inviteToGame

--- src/Old.hs
+++ src/Old.hs
@@ -4,11 +4,13 @@ module Old (oldMain) where
 
 import Data.List ((\\))
 
+import Alloc
 import Core
 import Lens
 import Lift
 import MainLoop
 import Monad
+import Parr
 import Propagate
 import Ty hiding (V, Bi, R)
 import qualified Ty as Ty
@@ -27,8 +29,9 @@ data W = W
   , invited :: [String]
   , joined :: [String]
   , declined :: [String]
+  , allocator :: Alloc
   }
-  deriving (Eq, Read, Show)
+  deriving (Read, Show)
 
 theWorld :: W
 theWorld = W
@@ -36,12 +39,14 @@ theWorld = W
   , invited = ["c@d.com"]
   , joined = []
   , declined = []
+  , allocator = mkAlloc
   }
 
 binvitees = field root "invitees" invitees $ \w invitees -> w { invitees }
 binvited = field root "invited" invited $ \w invited -> w { invited }
 bjoined = field root "joined" joined $ \w joined -> w { joined }
 bdeclined = field root "declined" declined $ \w declined -> w { declined }
+ballocator = field root "allocator" allocator $ \w allocator -> w { allocator }
 
 bListDiff :: Eq a => V [a] -> V [a] -> V [a]
 bListDiff = lift2 $ nuni "eq" (\\)
@@ -53,17 +58,18 @@ notYetInvited :: V [String]
 notYetInvited = bListDiff binvitees binvited
 
 lookupCommand :: AppEnv W
-lookupCommand ["old"] = old
+lookupCommand ["inviteToSite"] = inviteToSite
+lookupCommand ["inviteToGame"] = inviteToGameP ["a@b.com", "e@f.com", "g@h.com"]
 
 -- TODO don't want this Show a
 vappend :: Show a => Ty.V w [a] -> a -> Blef w ()
 vappend vas a = vas <--+ (++. (k [a]))
 
-old :: Program W
-old = toProg sdone old'
+inviteToSite :: Program W
+inviteToSite = toProg sdone inviteToSite'
 
-old' :: Blef W ()
-old' = do
+inviteToSite' :: Blef W ()
+inviteToSite' = do
   monitor notYetInvited $ \x -> msp ("nyi", x)
   monitor bjoined $ \x -> msp ("joined", x)
   monitor bdeclined $ \x -> msp ("declined", x)
@@ -76,7 +82,31 @@ old' = do
   inviteTheUninvited
   return ()
 
-data RSVP = Accept | Reject deriving (Read, Show)
+data RSVP = Accept | Reject deriving (Eq, Read, Show)
+
+inviteToGameP :: [String] -> Program W
+inviteToGameP emails = toProg sdone (inviteToGame emails)
+
+-- TODO should confirm they are in the user list
+inviteToGame :: [String] -> Blef W ()
+inviteToGame emails = do
+  results <- parrList ballocator (map invitePlayerToGame emails)
+  if all (==Accept) results
+    then startGame emails
+    else io $ msp $ "Someone doesn't want to play: " ++ (show $ zip emails results)
+
+invitePlayerToGame :: String -> Blef W RSVP
+invitePlayerToGame email = do
+  EBlef "game rsvp" $ \h -> do
+    let text = "sent to " ++ email ++ "\n" ++
+               "accept: " ++ acceptToken ++ "\n" ++
+               "reject: " ++ rejectToken ++ "\n"
+        acceptToken = show (Retval h "Accept")
+        rejectToken = show (Retval h "Reject")
+    msp text
+
+startGame :: [String] -> Blef W ()
+startGame emails = io $ msp $ "starting game with " ++ show emails
 
 invitePlayer :: String -> Blef W ()
 invitePlayer email = do
@@ -95,15 +125,21 @@ inviteTheUninvited :: Blef W ()
 inviteTheUninvited = do
   nyi <- BRead notYetInvited
   mapM_ (BFork . invitePlayer) nyi
-  io $ msp $ "gonna email" ++ (show nyi)
+  io $ msp $ "gonna email " ++ (show nyi)
 
 oldApp = App { initialW = theWorld, appEnv = lookupCommand }
 
 oldMain = do
   let dbdir = "old"
-  fromTheTop dbdir oldApp ["old"]
-  -- injectEvent dbdir oldApp (Retval 2 "Accept")
+
+  fromTheTop dbdir oldApp ["inviteToGame"]
+  injectEvent dbdir oldApp (Retval 0 "Accept")
+  injectEvent dbdir oldApp (Retval 1 "Accept")
   injectEvent dbdir oldApp (Retval 2 "Reject")
-  injectEvent dbdir oldApp (Retval 3 "Accept")
-  injectEvent dbdir oldApp (Retval 4 "Reject")
   run oldApp dbdir
+
+  -- fromTheTop dbdir oldApp ["inviteToSite"]
+  -- injectEvent dbdir oldApp (Retval 2 "Reject")
+  -- injectEvent dbdir oldApp (Retval 3 "Accept")
+  -- injectEvent dbdir oldApp (Retval 4 "Reject")
+  -- run oldApp dbdir
