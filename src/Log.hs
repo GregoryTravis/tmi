c4ba7d978aed6b53bb345ffaaa81a862643c2d5d
Gregory Travis <greg.m.travis@gmail.com>
2022-06-01 09:05:55 -0400

    fields, grabbing a call and event

--- src/Log.hs
+++ src/Log.hs
@@ -5,6 +5,8 @@ module Log
 
 import Unsafe.Coerce
 
+import Lens
+import Lift
 import Storage
 import Ty
 import V
@@ -20,16 +22,33 @@ data Log w = Log
   , logEvents :: [Event]
   }
   deriving (Read, Show)
+vlogCalls :: V WW [V (W App) (Call (W App))]
+vlogCalls = field vsysLog "logCalls" logCalls $ \w logCalls -> w { logCalls }
+vlogEvents = field vsysLog "logEvents" logEvents $ \w logEvents -> w { logEvents }
+
+grabCall :: V WW (V (W App) (Call (W App)))
+grabCall = vlogCalls !!. (VNice 0)
+
+grabEvent :: V WW Event
+grabEvent = vlogEvents !!. (VNice 0)
+
+(!!.) :: V w [a] -> V w Int -> V w a
+(!!.) = lift2 $ nuni "!!." (!!)
 
 data Sys w = Sys { sysLog :: Log w }
   deriving (Read, Show)
+vsysLog = field vwSys "sysLog" sysLog $ \w sysLog -> w { sysLog }
 
 data W app = W { wApp :: app, wSys :: Sys (W app) }
   deriving (Read, Show)
+vwApp = field vroot "wApp" wApp $ \w wApp -> w { wApp }
+vwSys = field vroot "wSys" wSys $ \w wSys -> w { wSys }
 
 data TMI w a = TMI a
 
 data Call w = forall a. Call (IO a) (a -> TMI w ())
+-- vcallK :: V w (Call w) -> V w (a -> TMI w ())
+-- vcallK = lift1 $ nuni "callK" (\(Call _ k) -> k)
 
 data Event = RetVal String -- | Command
   deriving (Eq, Ord, Read, Show)
