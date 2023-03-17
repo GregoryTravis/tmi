ad36b524592eb35503134454a0c63a86101857c6
Gregory Travis <greg.m.travis@gmail.com>
2023-03-17 17:52:02 -0400

    some rd wr conversion

--- src/Propagate.hs
+++ src/Propagate.hs
@@ -11,56 +11,69 @@ import Ty
 import Util
 import V
 
+data Generation = Latest | Frozen Int
+
 -- Pushes a write back one step through a rev?
-wr :: w -> V w b -> b -> Write w
-wr w (VBiSeal (Bi qfor qrev)) na =
-  let rev = rd w qrev -- R a
-      oa = rd w qfor -- a
+wr :: [w] -> Generation -> V w b -> b -> Write w
+wr ws gen (VBiSeal (Bi qfor qrev)) na =
+  let rev = rd ws gen qrev -- R a
+      oa = rd ws gen qfor -- a
    in case rev of R rec -> rec na
-wr w (VBiSeal bi) a =
-  let R rarec = getrev w bi
+wr ws gen (VBiSeal bi) a =
+  let R rarec = getrev ws gen bi
    in rarec a
-wr w q _ = error $ "wr?" -- ++ show q
+wr ws gen q _ = error $ "wr?" -- ++ show q
 
 -- vwr :: Show w => w -> V w b -> b -> Write w
 -- vwr w v b = eesp ("wr", v, w) $ wr w v b
 
-getrev :: w -> Bi w f r -> r
-getrev w (Bi qf qr) =
-  let r = rd w qr
+getrev :: [w] -> Generation -> Bi w f r -> r
+getrev ws gen (Bi qf qr) =
+  let r = rd ws gen qr
    in r
-getrev w (BiApp bi qa) =
-  let rev = getrev w bi
-      oa = rd w qa
+getrev ws gen (BiApp bi qa) =
+  let rev = getrev ws gen bi
+      oa = rd ws gen qa
       ra = R (\a -> Write qa a)
       rb = rev oa ra
    in rb
 
-rd :: w -> V w a -> a
-rd w VRoot = w
-rd w (VNice x) = x
-rd w (VNamed _ x) = x
-rd w (VBiSeal bi) = rdb w bi
-rd w (VDeref vva) = rd w (rd w vva)
-rd w (VFreeze _ v) = rd w v
+rd :: [w] -> Generation -> V w a -> a
+rd ws gen VRoot = w
+rd ws gen (VNice x) = x
+rd ws gen (VNamed _ x) = x
+rd ws gen (VBiSeal bi) = rdb [w] gen bi
+rd ws gen (VDeref vva) = rd [w] gen (rd [w] gen vva)
+rd ws gen (VFreeze _ v) = rd w v
 
-rdb :: w -> Bi w f r -> f
-rdb w (Bi qf qr) = rd w qf
-rdb w (BiApp bi qa) =
-  let for = rdb w bi
-      a = rd w qa
+rdb :: [w] -> Generation -> Bi w f r -> f
+rdb ws gen (Bi qf qr) = rd w qf
+rdb ws gen (BiApp bi qa) =
+  let for = rdb [w] gen bi
+      a = rd [w] gen qa
    in for a
 
-propWrite :: Show w => w -> Write w -> w
-propWrite w write = propWrites w [write]
+propWrite :: Show w => [w] -> Generation -> Write w -> w
+propWrite ws gen write = propWrites ws gen [write]
 
-propWrites :: Show w => w -> [Write w] -> w
-propWrites w [] = w
-propWrites w (VWrite va va' : rest) = propWrites w (Write va (rd w va') : rest)
-propWrites w (Write VRoot w' : rest) = propWrites w' rest
-propWrites w (Write va a : rest) = propWrites w (wr w va a : rest)
--- TODO append is slow?
-propWrites w (Writes writes : rest) = propWrites w (writes ++ rest)
+-- Apply a list of writes to a w. Writes are sequential.
+-- For a non-root write, the write is pushed back one step towards the root.
+-- For a root write, the new w replaces the old one.
+
+propWrites :: Show w => [w] -> Generation -> w -> [Write w] -> w
+propWrites ws gen w [] = w
+propWrites ws gen w (VWrite va va' : rest) = propWrites ws gen w (Write va (rd ws gen va') : rest)
+propWrites ws gen w (Write VRoot w' : rest) = propWrites ws gen w' rest
+propWrites ws gen w (Write va a : rest) = propWrites ws gen w (wr w va a : rest)
+
+-- Faulty attempt I think
+-- propWrites :: Show w => [w] -> Generation -> [Write w] -> w
+-- propWrites ws gen [] = w
+-- propWrites ws gen (VWrite va va' : rest) = propWrites ws gen (Write va (rd ws gen va') : rest)
+-- propWrites ws gen (Write VRoot w' : rest) = propWrites w' rest
+-- propWrites ws gen (Write va a : rest) = propWrites w (wr w va a : rest)
+-- -- TODO append is slow?
+-- propWrites w (Writes writes : rest) = propWrites w (writes ++ rest)
 
 -- Depth-first traversal, but updating the w we carry along each time we get a root write.
 -- This might actually be correct for cases where writes don't overlap? I don't know.
