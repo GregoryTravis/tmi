f0c3b87d353d99e1d3034cfbc3dd0fe6ba1d8564
Gregory Travis <greg.m.travis@gmail.com>
2025-09-27 13:24:42 -0400

    nested logging

--- src/Eval.hs
+++ src/Eval.hs
@@ -7,60 +7,63 @@ import History
 import Util
 import Val
 
+import Data.List (intercalate)
 import qualified Data.Map.Strict as M
 
-verbose = False
+verbose = True
 
 eval :: Interp -> Code -> Code
 eval interp@(Interp history _) code =
-  ep GlobalLayer code
+  ep 0 GlobalLayer code
   where
-    ev :: Env -> Code -> Code
-    ev env x =
-      let r = e env (eesp ("+eval", x) x)
-       in eesp ("-eval", x, r) r
-    ep :: Env -> Code -> Code
-    ep = if verbose then ev else e
-    e :: Env -> Code -> Code
-    e env l@(Lam arg body) = CVal $ dkv $ Closure env l
-    e env (Id id) =
+    ep :: Int -> Env -> Code -> Code
+    ep level = if verbose then ev level else e level
+    ev :: Int -> Env -> Code -> Code
+    ev level env x =
+      let pluses = intercalate "" (take level (repeat "+"))
+          minuses = intercalate "" (take level (repeat "-"))
+          r = e level env (eesp (pluses ++ " eval", x) x)
+       in eesp (minuses ++ " eval", x, r) r
+    e :: Int -> Env -> Code -> Code
+    e l env lm@(Lam arg body) = CVal $ dkv $ Closure env lm
+    e l env (Id id) =
       case elookup history env id of
-        Just x -> ep env $ CVal x
+        Just x -> ep (l+1) env $ CVal x
         Nothing ->
           error $ "Unknown identifier " ++ id
-    e env (CVal (Val _ (Code c))) =
-      e env c
-    e env (App (CVal (Val _ (Closure cenv (Lam arg body)))) x) =
-      let (CVal ex) = ep env x
+    e l env (CVal (Val _ (Code c))) =
+      ep (l+1) env c
+    e l env (App (CVal (Val _ (Closure cenv (Lam arg body)))) x) =
+      let (CVal ex) = ep (l+1) env x
           eenv = extend cenv arg ex
-       in ep eenv body
-    e env a@(App f x) =
-      ep env app'
-          where app' = App (ep env f) (ep env x)
-    e env (Builtin name args) =
-        CVal $ evalBuiltin interp name (map (unCVal . (ep env)) args)
-    e env (Ctor name args) =
-        CVal $ Val DK $ Cton name (map (unCVal . (ep env)) args)
-    e env (CtorRec name pairs) =
+       in ep (l+1) eenv body
+    e l env a@(App f x) =
+      ep (l+1) env app'
+          where app' = App (ep (l+1) env f) (ep (l+1) env x)
+    e l env (Builtin name args) =
+        CVal $ evalBuiltin interp name (map (unCVal . (ep (l+1) env)) args)
+    e l env (Ctor name args) =
+        CVal $ Val DK $ Cton name (map (unCVal . (ep (l+1) env)) args)
+    e l env (CtorRec name pairs) =
         let ids = map fst pairs
             exps = map snd pairs
-            vals = map (unCVal . (ep env)) exps
+            vals = map (unCVal . (ep (l+1) env)) exps
          in CVal $ Val DK $ CtonRec name (zip ids vals)
-    e env (If be th el) =
-        let b = e env be
+    e l env (If be th el) =
+        let b = ep (l+1) env be
          in case b of
-              CVal (Val _ (VB True)) -> e env th
-              CVal (Val _ (VB False)) -> e env el
+              CVal (Val _ (VB True)) -> ep (l+1) env th
+              CVal (Val _ (VB False)) -> ep (l+1) env el
               _ -> error $ "If: not a bool" ++ show b
-    e env (Case xc cases) =
-      let CVal x = e env xc
+    e l env (Case xc cases) =
+      let CVal x = ep (l+1) env xc
        in case match x cases of
             Nothing -> error $ "Pattern match failure: " ++ show x ++ " " ++ show cases
             Just (matchEnv, body) ->
               let env' = env <> matchEnv
-               in e env' body
+               in ep (l+1) env' body
     -- Eval to self
-    e _ x@(CVal _) = x
+    e _ _ x@(CVal _) = x
     -- e _ x = error $ "eval? " ++ show x
 
 evalBuiltin :: Interp -> Ident -> [Val] -> Val
